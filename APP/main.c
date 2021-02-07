d7a3ae49bb9dde21617dd79abe762c6ed70efaee
Ahmed Kishk <ahmedadelkishk@gmail.com>
2021-02-07 10:04:10 +0200

    Documenting code

--- APP/main.c
+++ APP/main.c
@@ -17,51 +17,49 @@
 #define EN_String_Length	11
 #define AR_String_Length	8
 
+/*	Function Prototype	*/
 void voidDisplayArName(void);
 
 int main(void) {
-	CLCD_voidInit();
-	CLCD_voidSendExtraChar();
-	CLCD_voidSetPosition(CLCD_ROW_1, CLCD_COL_1);
-	u8 LOC_u8String[] = "Ahmed Kishk";
-	u8 LOC_u8Iterator1 = 0;
-	s8 LOC_u8Iterator2 = 0;
-	u8 LOC_u8Counter = 0;
-	u8 LOC_u8flag = 0;
+	CLCD_voidInit();																//INITIALIZE LCD
+	CLCD_voidSendExtraChar();														//STORING SPECIAL CHARACTER (ARABIC) TO CGRAM
+	CLCD_voidSetPosition(CLCD_ROW_1, CLCD_COL_1);									//INITIALIZE CURSOR POSTION
+	u8 LOC_u8String[] = "Ahmed Kishk";												//DEFINE MY NAME IN ENGLISH
+	u8 LOC_u8Iterator1 = 0;															//DEFINE ITERATOR
+	s8 LOC_u8Iterator2 = 0;															//DEFINE ITERATOR
+	u8 LOC_u8Counter = 0;															//DEFINE ITERATOR
 	while (1) {
-		LOC_u8flag = 0;
-		for (LOC_u8Iterator1 = 1, LOC_u8Iterator2 = 16;
+		for (LOC_u8Iterator1 = CLCD_COL_1, LOC_u8Iterator2 = CLCD_COL_16;
 				LOC_u8Iterator1 <= LCD_COLS && LOC_u8Iterator2 >= ZERO;
 				LOC_u8Iterator1++, LOC_u8Iterator2--) {
 
-			CLCD_voidSetPosition(CLCD_ROW_1, LOC_u8Iterator1);
-			CLCD_voidMoveDirection(CLCD_LEFT_RIGHT);
-			CLCD_voidSendString(LOC_u8String);
-			CLCD_voidSetPosition(CLCD_ROW_2, LOC_u8Iterator2);
-			CLCD_voidMoveDirection(CLCD_RIGHT_LEFT);
-			voidDisplayArName();
-			_delay_ms(800);
-			CLCD_voidSetPosition(CLCD_ROW_1, LOC_u8Iterator1);
-			CLCD_voidSendString((u8 *) " ");
-			CLCD_voidSetPosition(CLCD_ROW_2, LOC_u8Iterator2);
-			CLCD_voidSendString((u8 *) " ");
+			CLCD_voidSetPosition(CLCD_ROW_1, LOC_u8Iterator1);						//SET CURSOR POSITION
+			CLCD_voidMoveDirection(CLCD_LEFT_RIGHT);								//SET CURSOR MOVING DIRECTION
+			CLCD_voidSendString(LOC_u8String);										//PRINT MY NAME IN THE LCD
+			CLCD_voidSetPosition(CLCD_ROW_2, LOC_u8Iterator2);						//SET CURSOR POSITION
+			CLCD_voidMoveDirection(CLCD_RIGHT_LEFT);								//SET CURSOR MOVING DIRECTION
+			voidDisplayArName();													//PRINT MY ARABIC NAME
+			_delay_ms(800);															//DELAY
+			CLCD_voidSetPosition(CLCD_ROW_1, LOC_u8Iterator1);						//SET CURSOR POSITION
+			CLCD_voidSendString((u8 *) " ");										//CLEAR THE FIRST CHARACTER IN MY ENGLISH NAME TO SHIFT
+			CLCD_voidSetPosition(CLCD_ROW_2, LOC_u8Iterator2);						//SET CURSOR POSITION
+			CLCD_voidSendString((u8 *) " ");										//CLEAR THE FIRST CHARACTER IN MY ARABIC NAME TO SHIFT
 
-			if (LOC_u8Iterator1 > LCD_COLS - EN_String_Length) {
-
-				CLCD_voidMoveDirection(CLCD_LEFT_RIGHT);
-				CLCD_voidSetPosition(CLCD_ROW_1, CLCD_COL_1);
-				for (LOC_u8Counter = LCD_COLS - LOC_u8Iterator1;
+			if (LOC_u8Iterator1 > LCD_COLS - EN_String_Length) {					//CHECK IF THE LAST CHARACTER ON MY NAME ARRIVED TO THE LAST COL
+				CLCD_voidMoveDirection(CLCD_LEFT_RIGHT);							//SET CURSOR MOVING DIRECTION LEFT TO RIGHT
+				CLCD_voidSetPosition(CLCD_ROW_1, CLCD_COL_1);						//SET CURSOR POSITION TO THE FIRST COL IN THE FIRST ROW
+				for (LOC_u8Counter = LCD_COLS - LOC_u8Iterator1;					//LOOP TO PRINT THE LAST CHARACTER FIRST ON MY NAME
 						LOC_u8Counter < EN_String_Length; LOC_u8Counter++) {
-					CLCD_voidSendData(LOC_u8String[LOC_u8Counter]);
+					CLCD_voidSendData(LOC_u8String[LOC_u8Counter]);					//PRINT CHARACTERS IN THE LCD
 
 				}
 
-				CLCD_voidMoveDirection(CLCD_RIGHT_LEFT);
-				CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_16);
+				CLCD_voidMoveDirection(CLCD_RIGHT_LEFT);							//SET CURSOR MOVING DIRECTION RIGHT TO LEFT
+				CLCD_voidSetPosition(CLCD_ROW_2, CLCD_COL_16);						//SET CURSOR POSITION TO THE LAST COL IN SECOND ROW
 				for (LOC_u8Counter = LCD_COLS - LOC_u8Iterator1;
-						LOC_u8Counter < AR_String_Length; LOC_u8Counter++) {
+						LOC_u8Counter < AR_String_Length; LOC_u8Counter++) {		//LOOP TO PRINT THE LAST CHARACTER FIRST ON MY NAME
 
-					CLCD_voidSendData(LOC_u8Counter);
+					CLCD_voidSendData(LOC_u8Counter);								//PRINT CHARACTER IN LCD
 
 				}
 
@@ -70,6 +68,9 @@ int main(void) {
 	}
 }
 
+
+
+/*	FUNCTION PROTOTYPE	*/
 void voidDisplayArName(void) {
 	for (u8 LOC_u8Iterator = 0; LOC_u8Iterator < AR_String_Length;
 			LOC_u8Iterator++) {
